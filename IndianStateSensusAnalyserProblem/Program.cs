9312a83a50ef99f5418c9636204de8ce8e5a5170
Divya <beingdivyadixit@gmail.com>
2023-02-01 17:37:10 +0530

    Added welcome Program

---
+++ IndianStateSensusAnalyserProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateSensusAnalyserProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Indian State Sensus Analyser Problem");
+            Console.ReadLine();
+        }
+    }
+}
